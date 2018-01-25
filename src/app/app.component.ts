5e3451ce02e85c162392cab17e42e27db0b18e91
Daniel Halperin <dan@intentionet.com>
2018-01-25 01:37:40 -0800

    Add ngx-tour-ng-bootstrap to ng-cli default project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TourService } from 'ngx-tour-ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private tourService: TourService, private router: Router) {
+    this.tourService.initialize([{
+      anchorId: 'some.anchor.id',
+      content: 'Some content',
+      title: 'First',
+    }, {
+      anchorId: 'another.anchor.id',
+      content: 'Other content',
+      title: 'Second',
+    }]);
+  }
+
+  ngOnInit() {
+    console.log('oninit');
+    this.tourService.start();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TourNgBootstrapModule } from 'ngx-tour-ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 
+const routing = RouterModule.forRoot([]);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    TourNgBootstrapModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
